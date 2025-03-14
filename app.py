f693e947ac13bb5465a306c0008a3e9f7983297b
tomoliveirabastos <washington-kd@hotmail.com>
2025-03-14 13:13:19 -0300

    add

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from services.mercado_bitcoin import MercadoBitcoin
+import sys
 
 app = FastAPI()
 
 # "from" é uma keyword do python, não é possivel usa-la como variavel
 @app.get("/{pair}/mms")
-async def index(from_timestamp: float, to_timestamp: float, range: int):
-    return {
-        "from": from_timestamp
-    }
+async def index(pair: str, from_timestamp: float, to_timestamp: float, range: int):
+    
+    try:
+        mb = MercadoBitcoin()
+        r = mb.api_candle_mb(pair=pair, range=range, from_timestamp=from_timestamp, to_timestamp=to_timestamp)
+
+        return r
+    
+    except BaseException as er:
+        return {
+            "error": str(er)
+        }
--- services/mercado_bitcoin.py
+++ services/mercado_bitcoin.py
@@ -33,27 +33,35 @@ class MercadoBitcoin:
         a cloudflare estava bloqueando as requests das libs requests e urllib3
         todo: ver o porque essas requests sao bloqueadas
         '''
-        r = os.popen(f"curl 'https://mobile.mercadobitcoin.com.br/v4/{pair}/candle?from={from_timestamp}&to={to_timestamp}&precision={range}d' \
+        url = f"curl 'https://mobile.mercadobitcoin.com.br/v4/{pair}/candle?from={int(from_timestamp)}&to={int(to_timestamp)}&precision={range}d' \
             -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:33.0) Gecko/20100101 Firefox/33.0' \
-            --compressed").read()
+            --compressed"
+        print(url)
+        
+        r = os.popen(url).read()
 
-        return loads(r)
+        j = loads(r)
 
+        if j['status_code'] != 100:
+            raise Exception(r)
+
+        return j
 
     def api_candle_mb(self, pair: str, range: int, from_timestamp: float, to_timestamp: float = None):
         
         if self.verifica_se_o_pair_esta_correto(pair) == False:
-            raise "pair precisa ser ['BRLBTC' ou 'BRLETH']"
+            raise Exception("pair precisa ser ['BRLBTC' ou 'BRLETH']")
 
         if self.verifica_timestamp_maior_que_365_dias(from_timestamp) == True:
-            raise "timestamp de from maior que 365 dias" 
+            raise Exception("timestamp de from maior que 365 dias")
 
         if to_timestamp == None:
             dd = datetime.now() - timedelta(days=1)
             to_timestamp = dd.timestamp()
 
         r = self.chamar_api_mercado_bitcoin(pair, range, from_timestamp, to_timestamp)
-        return self.calcular_mms(r['candles'], [20, 50, 200])
+
+        return self.calcular_mms(r['candles'], [20, 50, 200], pair)
 
 
     def retorna_media(self, valores: list[float]) -> float:
@@ -67,23 +75,22 @@ class MercadoBitcoin:
 
         return s / divisor
 
-    def calcular_mms(self, resultados_mb_api: list[dict], days:list[int]) -> list[MMSResult]:
+    def calcular_mms(self, resultados_mb_api: list[dict], days:list[int], pair: str = "BRLBTC") -> list[MMSResult]:
 
         l = []
 
         for k, r in enumerate(resultados_mb_api):
 
-            start_position = 0
-
             mms_result = MMSResult()
             mms_result.timestamp = r["timestamp"]
+            mms_result.pair = pair
 
             for day in days:
                 mms_result.__setattr__(f"mms_{day}", None)
 
                 if day > k:
                     continue                
-                
+                start_position = abs(k - day)
                 resultados_splice = resultados_mb_api[start_position:k]
                 m = self.retorna_media([x['close'] for x in resultados_splice])
                 mms_result.__setattr__(f"mms_{day}", m)
